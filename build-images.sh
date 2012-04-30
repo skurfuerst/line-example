b7af670c1bb6b39712e2cf43c1eb8c779dadd2de
Sebastian Kurfuerst <sebastian@typo3.org>
2012-04-30 12:18:46 +0200

    full image example

---
+++ build-images.sh
@@ -0,0 +1,14 @@
+export FLOW3_ROOTPATH="/Volumes/data/htdocs/Flow3Org"
+
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/black1.jpg images/black-multi-1 163 134 2211 99
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/black1.jpg images/black-single-1 130 359 2293 58
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/black1.jpg images/black-single-2 17 601 2401 96
+
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/blue1.jpg images/blue-multi-1 81 68 2318 101
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/blue1.jpg images/blue-multi-2 35 287 2409 86
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/blue1.jpg images/blue-single-1 35 714 2385 63
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/blue1.jpg images/blue-single-2 60 952 2339 88
+
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/red1.jpg images/red-multi-1 161 170 2099 95
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/red1.jpg images/red-single-1 185 727 2214 90
+$FLOW3_ROOTPATH/flow3 image:maketransparent raw/red1.jpg images/red-single-2 102 1110 2300 68
